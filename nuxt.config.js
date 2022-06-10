f079e2bcedb8020414500a0ea00958739716d0ba
shintoshio-me <shintoshio.me@gmail.com>
2022-06-10 23:26:18 +0800

    test routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
+    prefetchPayloads: false,
     base: '/swskillsimu-nuxt/'
   },
 
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <!-- Temporary i18n Switcher -->
     <div class="flex flex-row justify-center w-full mt-4 text-center text-sm text-white gap-2">
       <span v-for="locale in availableLocales" :key="locale.code">
-        <nuxt-link :to="switchLocalePath(locale.code) + '/'" class="bg-zinc-800 px-4 py-2 rounded-lg" prefetch v-text="locale.name" />
+        <nuxt-link :to="switchLocalePath(locale.code)" class="bg-zinc-800 px-4 py-2 rounded-lg" v-text="locale.name" />
       </span>
     </div>
 
